31966e0ecffd5b616eebbeb63d9b55133945669f
Andrew Burns <andrew@voicehost.co.za>
2023-11-18 07:32:51 +0200

    Adding tests.

--- tests/unit/libs/attempt.spec.js
+++ tests/unit/libs/attempt.spec.js
@@ -5,12 +5,12 @@ import * as fs from 'node:fs/promises'
 describe('Attempt Lib Test Suite', function () {
   // create test for attempt.mjs module
   it('Should return an error if the file does not exist', async function () {
-    const [error, result] = await attempt(fs.stat('./file-that-does-not-exist.mjs'))
+    const [error, result] = await attempt(Promise.reject(new Error('failure')))
     should().exist(error)
     should().not.exist(result)
   })
   it('Should return result if the file exists', async function () {
-    const [error, result] = await attempt(fs.stat('./package.json'))
+    const [error, result] = await attempt(Promise.resolve('success'))
     should().exist(result)
     should().not.exist(error)
   })
